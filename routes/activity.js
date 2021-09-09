caec1715afa179d7c033ee20477bec0f88a05a9a
Manikandanpashtek <86593624+Manikandanpashtek@users.noreply.github.com>
2021-09-09 11:11:03 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -136,7 +136,7 @@ exports.validate = function (req, res) {
 
 exports.insertRowForDCHelper = function (req, res) {
  let  deExternalKey = "DF34_Demo";
- let sfmcDataExtensionApiUrl = "https://www.exacttargetapis.com/hub/v1/dataevents/key:deExternalKey/rowset";
+ let sfmcDataExtensionApiUrl = "https://www.exacttargetapis.com/hub/v1/dataevents/key:"+deExternalKey+"/rowset";
   
  let headers = {
   'Content-Type': 'application/json;charset=UTF-8',
