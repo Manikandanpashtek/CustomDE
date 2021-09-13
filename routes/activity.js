f8658db64be272a80d8bdb229c59f033929ca09b
Manikandanpashtek <86593624+Manikandanpashtek@users.noreply.github.com>
2021-09-13 11:25:47 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -138,7 +138,7 @@ exports.insertRowForDCHelper = async (req, res) => {
  let  deExternalKey = "DF34_Demo";
  let url = req.query.endpoint;
  console.log("url",url);
- let sfmcDataExtensionApiUrl = "https://mcj6cy1x9m-t5h5tz0bfsyqj38ky.rest.marketingcloudapis.com/hub/v1/dataevents/key:DF34_Demo/rowset";
+ let sfmcDataExtensionApiUrl = "https://mcj6cy1x9m-t5h5tz0bfsyqj38ky.soap.marketingcloudapis.com//hub/v1/dataevents/key:DF34_Demo/rowset";
     let authToken = req.query.token
  console.log("authToken=",authToken);
  let jsonData = [
